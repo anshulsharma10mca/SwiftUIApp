1745e89c0e6fe2a2b02dcffc084db706036377c3
Anshul <Anshul.sharma10mca@gmail.com>
2021-02-27 22:01:00 -0500

    Add comments for SearchBar UI component.

--- SimpleUI/UIComponents/SearchBar/SearchBar.swift
+++ SimpleUI/UIComponents/SearchBar/SearchBar.swift
@@ -6,7 +6,12 @@
 //
 
 import SwiftUI
-
+/**
+ SearchBar is a reusable SwiftUI component,
+it's functionality is to take user's text input
+in search field and to publish any text change to
+it's observing entity (e.g. item's list in this app)
+*/
 public struct SearchBar {
     @ObservedObject
     var viewModel: ViewModel
